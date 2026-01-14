812ec2eaddaf0bfd488e5e9bae261e54cd3b883e
Bryan Caballero <mrid24013@cesunbc.edu.mx>
2026-01-14 00:21:08 -0800

    v01

--- datosGenerados.py
+++ datosGenerados.py
@@ -1,15 +1,18 @@
+#Se importan las bibliotecas que se van a utilizar para esta practica
 from faker import Faker
 import pandas as pd
 
+#Funcion que recibe los datos generados y los exporta por medio de Pandas en archivos .csv
 def exportarCSV(datos):
     exportar = pd.DataFrame(datos)
     exportar.to_csv('registros.csv', index=False)
     
-    
+#Creacion de nuestras variables iniciales
 fake = Faker()
 n = 50
 datos = {}
 
+#Datos generados por medio de un 'for', la cantidad de datos basada en la variable int 'n'
 for i in range(n):
     datos[i] = {
         'nombre': fake.name(),
@@ -18,4 +21,5 @@ for i in range(n):
         'usuario': fake.user_name()
     }
 
+#Se manda a llamar la funcion para exportar los datos generados
 exportarCSV(datos)
